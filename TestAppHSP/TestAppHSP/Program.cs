60abea1299ae4ff8ace5419bf2e7672b4f891f5b
richard668 <richard.weber@student.jade-hs.de>
2021-03-08 17:01:46 +0100

    Update Program.cs

--- TestAppHSP/TestAppHSP/Program.cs
+++ TestAppHSP/TestAppHSP/Program.cs
@@ -6,7 +6,7 @@ namespace TestAppHSP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hallo!");
         }
     }
 }
